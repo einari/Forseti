2e4ffc1302072734a4fd950fded0b0fa611b5802
Einar Ingebrigtsen <einar@dolittle.com>
2011-10-10 14:15:54 +0200

    Missing Specs

---
+++ Source/Forseti.Specs/Configuration/for_Configure/FakeFramework.cs
@@ -0,0 +1,8 @@
+﻿namespace Forseti.Specs.Configuration.for_Configure
+{
+    public class FakeFramework : IFramework
+    {
+        public string Script { get; set; }
+        public string ExecuteScript { get; set; }
+    }
+}
