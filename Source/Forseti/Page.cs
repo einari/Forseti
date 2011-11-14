be69743e1cec019caab181a35a317fdbeb940cbe
Einar Ingebrigtsen <einar@dolittle.com>
2011-11-14 21:25:36 +0100

    Prototype up and running - it actually works.. :)

--- Source/Forseti/Page.cs
+++ Source/Forseti/Page.cs
@@ -2,6 +2,7 @@
 {
     public class Page
     {
+        public string RootPath { get; set; }
         public string Filename { get; set; }
         public string Content { get; set; }
     }
--- Source/Forseti/Spark/PageGenerator.cs
+++ Source/Forseti/Spark/PageGenerator.cs
@@ -2,6 +2,7 @@
 using Forseti.Resources;
 using Spark;
 using Spark.FileSystem;
+using System.Text;
 
 namespace Forseti.Spark
 {
@@ -36,13 +37,43 @@ namespace Forseti.Spark
 
             var harnessView = (HarnessView)_engine.CreateInstance(_descriptor);
             harnessView.Harness = harness;
+            harnessView.FrameworkScript = _framework.ScriptName;
+            harnessView.FrameworkExecutionScript = _framework.ExecuteScriptName;
+            harnessView.FrameworkReportingScript = _framework.ReportScriptName;
+
             var writer = new StringWriter();
             harnessView.RenderView(writer);
 
             var result = writer.ToString();
-            File.WriteAllText("c:\\jasmine-runner.html", result);
+
+            page.RootPath = "c:\\";
+
+            File.WriteAllText(page.RootPath + _framework.ScriptName, _framework.Script);
+            File.WriteAllText(page.RootPath + _framework.ExecuteScriptName, _framework.ExecuteScript);
+            File.WriteAllText(page.RootPath + _framework.ReportScriptName, _framework.ReportScript);
+
+
+            foreach (var scriptFile in harnessView.SystemScripts)
+                CopyScript(page.RootPath, scriptFile);
+                //File.Copy(scriptFile, page.RootPath + scriptFile, true);
+
+            foreach (var scriptFile in harnessView.CaseScripts)
+                CopyScript(page.RootPath, scriptFile);
+                //File.Copy(scriptFile, page.RootPath + scriptFile, true);
+            
+
+            page.Filename = "c:\\jasmine-runner.html";
+
+            File.WriteAllText(page.Filename, result);
 
             return page;
+        }
+
+
+        void CopyScript(string rootPath, string scriptFile)
+        {
+            var script = File.ReadAllText(scriptFile);
+            File.WriteAllText(rootPath + scriptFile, script, Encoding.ASCII);
         }
     }
 }
