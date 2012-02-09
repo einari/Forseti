a769a8060b92bfa03605be2dcc2bbdabb8db5337
pavsaund <pav.saund@gmail.com>
2012-02-09 23:47:36 +0100

    Added generic parsing of system console methods to be made availabe for the test-runner.

--- Source/Forseti/Scripting/ScriptEngineContext.cs
+++ Source/Forseti/Scripting/ScriptEngineContext.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using Forseti.Resources;
+using Forseti.Scripting.Extensions;
 using java.lang;
 using java.lang.reflect;
 using org.mozilla.javascript;
@@ -19,14 +22,39 @@ namespace Forseti.Scripting
             _context.setOptimizationLevel(-1);
             _scope = _context.initStandardObjects();
 
-            Class myJClass = typeof(SystemConsole);
-            Member method = myJClass.getMethod("Print", typeof(string));
-            Scriptable function = new FunctionObject("print", method, _scope);
-            _scope.put("print", _scope, function);
+            AddConsoleFunctionsToScope();
 
             SystemConsole.LoggingEnabled = false;
             _context.evaluateString(_scope, envJs, "env.js", 1, null);
             SystemConsole.LoggingEnabled = true;
+        }
+
+        void AddConsoleFunctionsToScope()
+        {
+            var consoleLoggingMethods = typeof (SystemConsole).GetMethods(BindingFlags.Public | BindingFlags.Static);
+            Class myJClass = typeof (SystemConsole);
+            foreach (var method in consoleLoggingMethods)
+            {
+                Member methodMember = myJClass.getMethod(method.Name, GetParamatersForMethod(method));
+                var functionName = method.Name.ToCamelCase();
+                Scriptable methodFunction = new FunctionObject(functionName, methodMember, _scope);
+                _scope.put(functionName,_scope, methodFunction);
+
+            }
+        }
+
+        Class[] GetParamatersForMethod(MethodInfo method)
+        {
+            var listOfParamTypes = new List<Class>();
+            var parameters = method.GetParameters();
+
+            foreach (var parameterInfo in parameters)
+            {
+
+                listOfParamTypes.Add(parameterInfo.ParameterType);  
+                
+            }
+            return listOfParamTypes.ToArray();
         }
 
         public void EvaluateString(string script, string source)
