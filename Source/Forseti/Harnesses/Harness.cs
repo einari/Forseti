94bac6050ff79180999f890a86c78951f1fc1b7e
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-12 08:05:00 +0100

    Started working on the Harness and the checking of files against the configured search path

--- Source/Forseti/Harnesses/Harness.cs
+++ Source/Forseti/Harnesses/Harness.cs
@@ -1,17 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Forseti.Suites;
 
 namespace Forseti
 {
     public class Harness
     {
+		string _systemsSearchPath;
+		string _descriptionsSearchPath;
+		Regex _systemsSearchPathRegex;
+		Regex _descriptionsSearchPathRegex;
+		
 		public string Name { get; set; }
-		public string SystemsSearchPath { get; set; }
-		public string DescriptionsSearchPath { get; set; }
+		public string SystemsSearchPath 
+		{ 
+			get { return _systemsSearchPath; }
+			set
+			{
+				_systemsSearchPath = value;
+			}
+		}
+		
+		public string DescriptionsSearchPath 
+		{ 
+			get { return _descriptionsSearchPath; }
+			set
+			{
+				_descriptionsSearchPath = value;
+			}
+		}
+		
+		
+		Regex BuildSearchRegex() 
+		{
+			var pattern = string.Empty;
+			
+			return new Regex(pattern);
+		}
+		
 		
 		public IEnumerable<Suite> Suites { get; set; }
         public IEnumerable<Case> Cases { get; set; }
 		
+		public bool IsSystemFile(string relativePath)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public bool IsDescriptionsFile(string relativePath)
+		{
+			throw new NotImplementedException();
+		}
 		
 		
     }
