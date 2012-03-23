21400a50ef3d3553e8a0a4177641c5b4a6e15192
Einar Ingebrigtsen <einar@dolittle.com>
2012-03-23 21:13:41 +0100

    Started fleshing QUnit support

---
+++ Source/Forseti.QUnit/ConfigureExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Forseti.Configuration
+{
+    public static class ConfigureExtensions
+    {
+        public static IConfigure UsingQUnit(this IConfigure configure)
+        {
+            configure.Container.Configure(
+                c => 
+                    c   .For<IFramework>()
+                        .Singleton()
+                        .Use<QUnit.Framework>());
+            return configure;
+        }
+    }
+}
---
+++ Source/Forseti.QUnit/Framework.cs
@@ -0,0 +1,25 @@
+﻿using Forseti.Resources;
+
+namespace Forseti.QUnit
+{
+    public class Framework : IFramework
+    {
+        public Framework(IResourceManager resourceManager)
+        {
+            ScriptName = "qunit.js";
+            Script = resourceManager.GetStringFromAssemblyOf<Framework>("Forseti.QUnit.Scripts.qunit.js");
+            ExecuteScriptName = "qunit-executor.js";
+            ExecuteScript = resourceManager.GetStringFromAssemblyOf<Framework>("Forseti.QUnit.Scripts.qunit-executor.js");
+            ReportScriptName = "qunit-reporter.js";
+            ReportScript = resourceManager.GetStringFromAssemblyOf<Framework>("Forseti.QUnit.Scripts.qunit-reporter.js");
+        }
+
+
+        public string ScriptName { get; private set; }
+        public string Script { get; private set; }
+        public string ExecuteScriptName { get; private set; }
+        public string ExecuteScript { get; private set; }
+        public string ReportScriptName { get; private set; }
+        public string ReportScript { get; private set; }
+    }
+}
