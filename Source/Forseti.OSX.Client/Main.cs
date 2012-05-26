70b1f86fedf46d1c00430f9a2c5da47dde8c02a5
Einar Ingebrigtsen <einar@dolittle.com>
2012-05-26 20:45:17 +0200

    Initial OSX Client work started with Growl support

---
+++ Source/Forseti.OSX.Client/Main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Runtime;
+using System.IO;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace Forseti.OSX.Client
+{
+	class MainClass
+	{
+		static void Main (string[] args)
+		{
+			var baseAppPath = Directory.GetParent (Directory.GetParent (System.AppDomain.CurrentDomain.BaseDirectory).ToString ());
+			//var growlPath = baseAppPath + "/Frameworks/Growl-WithInstaller.framework/Growl-WithInstaller";
+			var growlPath = baseAppPath + "/Frameworks/Growl.framework/Growl";
+			
+			Dlfcn.dlopen (growlPath, 0);
+
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}	
+
