452ea95088d913a43b742bce3d0fed82d0280471
Einar Ingebrigtsen <einar@dolittle.com>
2012-11-25 14:07:25 +0100

    Started working on TFS Activity for running Forseti

---
+++ Source/Forseti.TFSBuildActivities/HarnessWatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Forseti.Extensions;
+using Forseti.Harnesses;
+using Forseti.Suites;
+
+namespace Forseti.TFSBuildActivities
+{
+    public class HarnessWatcher : IHarnessWatcher
+    {
+        public void HarnessChanged(HarnessChangeType changeType, HarnessResult harness)
+        {
+            harness.AffectedSuites.ForEach(
+                suite => suite.Descriptions.ForEach(description =>
+                {
+                    description.Cases.ForEach(@case =>
+                    {
+                        if (CanResultBeReported(@case))
+                            return;
+
+                        //if (@case.Result.Success)
+                            //PrintPassedCase(@case);
+                        //else
+                            //PrintFailedCase(@case);
+                    });
+                }));
+
+        }
+
+        static bool CanResultBeReported(Case @case)
+        {
+            return String.IsNullOrEmpty(@case.Name);
+        }
+
+    }
+}
---
+++ Source/Forseti.TFSBuildActivities/RunJavaScriptDescriptionsFromYaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Activities;
+using System.IO;
+using Forseti.Configuration;
+using Microsoft.TeamFoundation.Build.Client;
+
+namespace Forseti.TFSBuildActivities
+{
+    [BuildActivity(HostEnvironmentOption.All)]
+    public sealed class RunJavaScriptDescriptionsFromYaml : CodeActivity
+    {
+        public InArgument<string> YamlFile { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var file = context.GetValue(this.YamlFile);
+            var fileDirectory = Path.GetDirectoryName(file);
+            Directory.SetCurrentDirectory(fileDirectory);
+
+
+            var configuration = Configure
+                .WithStandard()
+                .FromConfigurationFile(file)
+                .Initialize();
+
+            configuration.HarnessChangeManager.RegisterWatcher(typeof(HarnessWatcher));
+
+            configuration
+                    .HarnessManager.Run();
+
+            Directory.SetCurrentDirectory(currentDirectory);
+        }
+    }
+}
