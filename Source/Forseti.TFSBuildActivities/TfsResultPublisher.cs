17c994abfce9a491f02a6564cdb04f5470efbc0d
Pavneet Singh Saund <pavneet@dolittle.com>
2012-12-11 21:48:20 +0100

    Added MSTEst.exe lookup logic. Custom hint can be given if needed

--- Source/Forseti.TFSBuildActivities/TfsResultPublisher.cs
+++ Source/Forseti.TFSBuildActivities/TfsResultPublisher.cs
@@ -14,16 +14,42 @@ namespace Forseti.TFSBuildActivities
         string _teamProjectUrl;
         string _buildNumber;
         string _teamProjectName;
+        string _platform;
+        string _flavor;
+
+
+        string[] msTestPathHints = new[] { Environment.ExpandEnvironmentVariables("%VS100COMNTOOLS%\\..\\IDE\\MSTest.exe"),
+                                            Environment.ExpandEnvironmentVariables("%ProgramFiles(X86)%\\Microsoft Visual Studio 10.0\\Common7\\MSTest.exe"),
+                                            Environment.ExpandEnvironmentVariables("%ProgramFiles%\\Microsoft Visual Studio 10.0\\Common7\\MSTest.exe"), 
+                                            "C:\\Program Files\\Microsoft Visual Studio 10.0\\Common7\\IDE\\MSTest.exe",
+                                            "C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\Common7\\IDE\\MSTest.exe"};
+
 
         public Action<string> Log { get; set; }
 
-        public TfsResultPublisher(string teamProjectUrl, string buildNumber, string teamProjectName)
+        public TfsResultPublisher(string teamProjectUrl, string buildNumber, string teamProjectName, string platform, string flavor, string msTestPathHint = "")
         {
             _teamProjectUrl = teamProjectUrl;
             _buildNumber = buildNumber;
             _teamProjectName = teamProjectName;
+            _platform = platform;
+            _flavor = flavor;
+
+            if (!string.IsNullOrEmpty(msTestPathHint))
+                msTestPathHint.Insert(0, msTestPathHint);
 
             Log = (s) => Console.WriteLine(s); 
+        }
+
+        private string LocateMstestPath()
+        {
+            foreach (var alternative in msTestPathHints)
+            {
+                if(File.Exists(Path.GetFullPath(alternative)))
+                    return alternative;
+            }
+
+            throw new FileNotFoundException("Could not locate MSTest.exe.");
         }
 
         public void PublishResultsFromPath(string trxFilePath)
@@ -34,11 +60,11 @@ namespace Forseti.TFSBuildActivities
             if (!File.Exists(filePath))
                 throw new FileNotFoundException("Could not locate the trx to be published to the build at following path : {0}", trxFilePath);
 
-            var mstestPath = Environment.ExpandEnvironmentVariables(@"C:\Program Files (X86)\Microsoft Visual Studio 10.0\Common7\IDE\MSTest.exe");//("%VS100COMNTOOLS%\\..\\IDE\\MSTest.exe");
+            var mstestPath = LocateMstestPath();
             Log(string.Format("MSTEST Path : {0}", mstestPath));
 
-            var arguments = string.Format(@"/publish:{0} /publishbuild:{1} /platform:{2} /flavor:{3} /teamproject:{4} /publishresultsfile:{5}",
-                                                    _teamProjectUrl, _buildNumber, "\"x86\"", "Debug", _teamProjectName, trxFilePath);
+            var arguments = string.Format("/publish:{0} /publishbuild:{1} /platform:\"{2}\" /flavor:\"{3}\" /teamproject:{4} /publishresultsfile:{5}",
+                                                    _teamProjectUrl, _buildNumber, _platform, _flavor, _teamProjectName, trxFilePath);
            Log(string.Format("MSTEST Arguments : {0}", arguments));
 
             // Command-Line Options For puvlishing Test Results: http://msdn.microsoft.com/en-us/library/ms243151.aspx
