7202ba3c50364b9792022197341ffed545c6457a
Pavneet Singh Saund <pavneet@dolittle.com>
2012-12-18 22:37:01 +0100

    implemented the forseti.framework for jasmine

--- Source/Forseti.Jasmine/Scripts/jasmine-executor.js
+++ Source/Forseti.Jasmine/Scripts/jasmine-executor.js
@@ -1,6 +1,22 @@
 ﻿(function () {
     //forseti.log(something);
-    var jasmineEnv = jasmine.getEnv();
-    jasmineEnv.updateInterval = 1000;
-    jasmineEnv.execute();
+
+    if (!forseti) {
+        throw new Exception();
+    }
+
+    var framework = forseti.framework;
+    framework.initialize = function () {
+        if (!framework.instance) {
+            var jasmineEnv = jasmine.getEnv();
+            jasmineEnv.updateInterval = 1000;
+            framework.instance = jasmineEnv;
+            framework.instance.execute();
+        }
+    };
+
+    framework.execute = function () {
+//        if (framework.instance)
+//            framework.instance.execute();
+    }
 })();
